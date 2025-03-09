62957ab2c35972086b658d5ddd37ca762cdb08f1
RTCupid <19apr2006@vk.com>
2025-03-09 16:22:45 +0300

    working hacking, Start to make graphics

--- main.cpp
+++ main.cpp
@@ -11,25 +11,29 @@ int main ()
 
     char* HackBuffer = NULL;
 
-    struct MP Ongn
+    struct MP Ongn                                                  // buffer's structure
     {
         &HackBuffer,
         0,
         "DimaMorg/CRACK.COM",
     };
 
-    if (!InputOnegin (&Ongn))
-    {
-        printf (RED "ERROR: CAN'T OPEN FILE" RESET);
+    if (!InputOnegin (&Ongn))                                       // func to find len of file
+    {                                                               // and make buffer from file
+        printf (RED "ERROR: CAN'T MAKE BUFFER FROM FILE" RESET);
         return 0;
     }
-    printf (GRN "%s\n" RESET, *Ongn.HackBuffer);
 
-    (*Ongn.HackBuffer)[97] = char(144);                               // jz -> jmp in CRACKME.COM
+    printf (GRN "%s\n" RESET, *Ongn.HackBuffer);                    // this operation doesn't work correctly
+
+    (*Ongn.HackBuffer)[103] = char(104);                            // call -> push in CRACKME.COM
+    (*Ongn.HackBuffer)[104] = char(107);                            // ptr to func that
+    (*Ongn.HackBuffer)[105] = char(1);                              // gives admin rights
+                                                                    // because next operator is ret
 
     FILE* Hacked_file = fopen ("DimaMorg/HACKED_CRACKME.COM", "w");
 
-    fwrite (*Ongn.HackBuffer, sizeof (char), Ongn.sizeOfFile, Hacked_file);
+    fwrite (*Ongn.HackBuffer, sizeof (char), Ongn.sizeOfFile, Hacked_file); // write hacking code to new file
 
     fclose (Hacked_file);
 
